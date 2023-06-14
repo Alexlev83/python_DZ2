2d2dc78471673f5ab6552e40ff102c4c1bafeeaa
Aleksandr <alexlev83@list.ru>
2023-06-14 10:26:21 +0300

    ПЕ

--- python_DZ7.py
+++ python_DZ7.py
@@ -1,18 +1,3 @@
-# 7.1[34]: Винни-Пух попросил Вас посмотреть, есть ли в его стихах ритм. Поскольку разобраться 
-# в его кричалках не настолько просто, насколько легко он их придумывает, Вам стоит написать программу. 
-# Винни-Пух считает, что ритм есть, если число слогов (т.е. число гласных букв) в каждой фразе стихотворения 
-# одинаковое. Фраза может состоять из одного слова, если во фразе несколько слов, то они разделяются дефисами. 
-# Фразы отделяются друг от друга пробелами.
-# Написать функцию, которая принимает строку текста и проверяет ее ритм (по Винни-Пуху)
-# Если ритм есть, функция возвращает True, иначе возвращает False
-
-# Примеры/Тесты:
-#     <function_name>("пара-ра-рам рам-пам-папам па-ра-па-дам") -> True
-#     <function_name>("пара-ра-рам рам-пум-пупам па-ре-по-дам") -> True
-#     <function_name>("пара-ра-рам рам-пуум-пупам па-ре-по-дам") -> False
-#     <function_name>("Трам-пара-папам-парам-па-пам-пам-па Пум-пурум-пу-пурум-трам-пам-па") -> False
-#     <function_name>("Пам-парам-пурум Пум-пурум-карам") -> True
-
 
 # Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
 # которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру 
@@ -33,12 +18,19 @@
 # 4   8  12  16  20  24
 # 5  10  15  20  25  30
 # 6  12  18  24  30  36
+# def print_operation_table(operation,num_rows,num_columns):   
+#     for i in range(1,num_rows+1):
+#         print()
+#         for j in range(1,num_columns+1):
+#             print(operation(i,j), end=" ")
 
-def print_operation_table(operation, num_rows=6, num_columns=6):
+# print_operation_table(lambda x, y: x ** y,4,4)
 
- if len(num_rows) == len(num_columns):
-  for idx in range(1, len(num_rows)):
-   for idx in range(1, len(num_columns)):
-    if operation(num_rows[idx]) == operation(num_columns[idx]):
-     return num_rows*num_columns
-print(print_operation_table(lambda x,y: x*y))
+
+def print_operation_table(operation,num_rows,num_columns):   
+    for i in range(1,num_rows+1):
+        print()
+        for j in range(1,num_columns+1):
+            print(operation(i,j), end=" ")
+
+print_operation_table(lambda x, y: x * y,6,6)
