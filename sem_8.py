b16134b1a4f9fcddecd353877cd981efbea1d658
Aleksandr <alexlev83@list.ru>
2023-06-19 09:36:59 +0300

    Справочник

--- sem_8.py
+++ sem_8.py
@@ -21,65 +21,124 @@
 # user=["ferstname","secondname","phone","discription"]
 # dictionary = {1:["ferstname","secondname","phone","discription"],2:["ferstname","secondname","phone","discription"]}
 
-def Input_Users()->list:
+
+def Input_user()->list:
     user=[]
     user.append(input("Input first name "))
     user.append(input("Input second name "))
     user.append(input("Input phone "))
     user.append(input("Input discription "))
-    
     return user
-# print(Input_Users())
-# key_count = 0
-# phone_dir = dict()
+
 def create(phone_dir_local: dict, idc: int, user:list)->dict:
     idc += 1
     phone_dir_local[idc] = user
     return phone_dir_local, idc
+
 user1 = ["second_name1","first_name1","phone1","discription1"]
 user2 = ["second_name2","first_name2","phone2","discription2"]
 
-# phone_dir, key_count=create(phone_dir,key_count,user1)
-# phone_dir, key_count=create(phone_dir,key_count,user2)
-# print(phone_dir)
-
-def menu ():
-    print("Введите 1, если хотите ввести пользователя ")
-    print("Введите 2, если хотите распечатать справочник ")
-    print("Введите 3, для экспорта ")
-    key_count = 0
-    phone_dir = dict()
-    while True:
-        num = int(input("Выберите операцию "))
-        if num == 0:
-            break
-        if num == 1:
-           user = Input_Users()
-           phone_dir, key_count = create(phone_dir,key_count,user)
-        if num == 2:
-            print (phone_dir)
-        if num == 3:
-            file_name = input("Введите имя файла ")
-            export_phone_dir(phone_dir, file_name)
 
 def export_phone_dir(phone_dir:dict, file_name):
  MAIN_DIR = abspath(dirname(__file__))
  full_name = join(MAIN_DIR, file_name+ '.txt')
  with open(full_name, mode = 'w', encoding = 'utf-8') as file:
      for idc, user in phone_dir.items():
-         file.write(f"{idc},{user[0]},{user[1]},{user[2]},{user[3]}\n")
+         file.write(f"{idc}#{user[0]}#{user[1]}#{user[2]}#{user[3]}\n")
+
+def import_dir(phone_dir_local: dict, file_name: str, idc: int):
+    with open (file_name, "rt") as dir_from_file:
+        for item in dir_from_file: 
+            idc+=1
+            temp_user = item.strip().split("#") 
+            phone_dir_local [idc] = temp_user[1:]
+            return phone_dir_local, idc
 
 def search_user(phone_dir: dict, searching: str) ->int:
              for idc, user in phone_dir.items():
                  if(user[0]).startswith(searching):
                      return idc
-                 
-
 def print_dict(phone_dir: dict):
     for idc, user in phone_dir.items():
-        print(f"{idc}:{user[0]} {user[1]} {user[2]} {user[3]}\n")
+        print(f"{idc}:{user[0]} {user[1]} {user[2]} {user[3]}")
+
+def delete (phone_dir: dict, searching: str):
+ 
+    tmp = search_user(phone_dir, searching)
+    if tmp is not None:
+        idc, user = tmp
+        phone_dir.pop(idc)
+    else:
+        print ("Пользователь не найден")
+    return phone_dir
+
+def update (phone_dir: dict, searching: str): 
+      tmp = search_user(phone_dir, searching)
+      if tmp is None:
+         print ("Пользователь не найден")
+         return phone_dir
+
+      idc, _ = tmp
+      new_second_name = input ("Заменить фамилию") 
+      new_name = input ("Заменить имя")
+      new_phone = input ("Заменить номер")
+      new_discrition = input ("Заменить описание")
+      if len(new_second_name) >=1:
+            phone_dir [idc][0] = new_second_name
+      if len(new_name) >=1:
+            phone_dir [idc][1] = new_name
+      if len(new_phone) >=1:
+            phone_dir [idc][2] = new_phone
+      if len(new_discrition) >=1:
+            phone_dir [idc][3] = new_discrition
+      else:
+            print ("Пользователь не найден")
+      return phone_dir
+
+def menu (phone_dir:dict):
+    print("Введите 1, если хотите ввести пользователя ")
+    print("Введите 2, если хотите распечатать справочник ")
+    print("Введите 3, для экспорта ")
+    print("Введите 4 для поиска")
+    print("Введите 5, удаление")
+    print("Введите 6, редактирование")
+    print("Введите 7, импортировать")
+    key_count = 0
+    phone_dir = dict()
+    number = 0
+    fio = ""
+    phone_dir = dict()
+    while True:
+        num = int(input("Выберите операцию "))
+        if num == 0:
+            break
+        if num == 1:
+           user = Input_user()
+           phone_dir, key_count = create(phone_dir,key_count,user)
+        if num == 2:
+            print (phone_dir)
+        if num == 3:
+            file_name = input("Введите имя файла")
+            export_phone_dir(phone_dir, file_name)
+        if num == 4:
+            searching = input("Кого ищем?")
+            number, fio = search_user(phone_dir, searching)
+            print (f"Пользователь {fio} номер {number}")
+        if num == 5: 
+            remove_name = input("Кого удалить?")
+            phone_dir = delete (phone_dir, remove_name )
+            print ("Удален")
+        if num == 6: 
+            change_element = input("Кого редактируем?")
+            phone_dir = update (phone_dir, change_element)
+            print (phone_dir)
+        if num == 7: 
+            import_file_name = input("Введите название файла для импорта")
+            phone_dir, key_count = import_dir (phone_dir, import_file_name,key_count)
+            print(phone_dir)
+      
 from os.path import join, abspath, dirname
-phone_dir = {
+phone_direct = {
 1: ['Иванов',   'Иван',  '+7(xxx)xxx-xx-xx', 'desription_Иванов'], 
 2: ['Петров',   'Петр',  '+7(---)xxx-xx-xx', 'desription_Петров'], 
 3: ['Соколов',  'Илья',  '+7(---)---------', 'desription_Соколов'], 
@@ -88,14 +147,3 @@ phone_dir = {
 6: ['Сааков',   'Илья',  '+7(+++)+++-++-++', 'desription_Сааков'],  
 }
 
-print_dict(phone_dir)
-
-def creating():
-    file = 'Phonebook.csv'
-    with open(file, 'w', encoding='utf-8') as data:
-        data.write(f'Фамилия;Имя;Номер телефона;Описание\n')
-
-def from_file(file):
-    with open(file, 'r', encoding='utf-8') as data:
-        dictionary = data.read()
-    return dictionary
